7a7c616b26bddeef208588d298fba58ac68e0fe5
Andrej Nenadić <andrejnana445@gmail.com>
2024-03-31 00:07:25 +0100

    Initial commit

---
+++ src/Components/SongDisplay.tsx
@@ -0,0 +1,11 @@
+import "./SongDisplay.scss";
+
+export default function SongDisplay() {
+  return (
+    <div className="song-display">
+      <h1>Song name</h1>
+      <h2>Artist name</h2>
+      <h3>Number of listens</h3>
+    </div>
+  );
+}
